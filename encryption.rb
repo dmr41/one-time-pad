475833acc7cbde9deddc3ff67abaa3a5011ae7bc
David Rivers <dmr41.rivers@gmail.com>
2015-06-01 21:47:32 +0200

    encryption with one time pad is working - need more test to verify edge cases

--- encryption.rb
+++ encryption.rb
@@ -1,12 +1,13 @@
 
 class Encryption
-	attr_accessor :phrase, :one_time_phrase
+	attr_accessor :starting_phrase, :one_time_phrase, :encrypted_message
 
 ## Encryption class initializes phrase instance variable provided by user
 ## Default state is an empty string
 	def initialize (phrase_to_encode="")
-		@phrase = phrase_content(phrase_to_encode)
+		@starting_phrase = phrase_content(phrase_to_encode)
 		@one_time_phrase = ""
+		@encrypted_message = ""
 	end
 
 ## phrase_content takes user input and tests if it is a string and
@@ -15,28 +16,39 @@ class Encryption
 		if (phrase_to_encode.is_a?(String) && phrase_to_encode != "" )
 			phrase_to_encode.gsub!(/\s+/, "")
 			if(phrase_to_encode[/[a-zA-Z]+/] == phrase_to_encode)
-				@phrase = phrase_to_encode.upcase
+				@starting_phrase = phrase_to_encode.upcase
 			end
 		else
 			puts "Your code is not a string between A-Z"
 		end
-			@phrase ||= ""
+			@starting_phrase ||= ""
 	end
 
 ## Generate random phrase from user phrase input
 	def randomize_phrase
-
-		if(@phrase.length)
-			for letter in  1..@phrase.length
-				letter_roulet = (65+ rand(25)).chr
+		if(@starting_phrase.length)
+			for letter in  1..@starting_phrase.length
+				letter_roulet = (65 + rand(25)).chr
 				@one_time_phrase += letter_roulet
 			end
-			puts "Starting phrase: #{@phrase}"
-			puts "One time:        #{@one_time_phrase}"
 		else
 			puts "Phrase is empty. Please try again!"
 		end
 		@one_time_phrase
 	end
 
+	def encrypt_phrase
+		unencoded_array = @starting_phrase.split('').map(&:ord)
+		one_time_array = @one_time_phrase.split('').map(&:ord)
+		encrypted_array = []
+		unencoded_array.each_with_index do |starting_value, index|
+			one_time_value_adjusted = one_time_array[index] - 65
+			starting_value_adjusted = starting_value - 65
+			encoding_value = starting_value_adjusted + one_time_value_adjusted - 1
+			@encrypted_message += ((encoding_value % 26) + 65).chr
+		end
+		puts "Starting phrase: #{@starting_phrase}"
+		puts "One time:        #{@one_time_phrase}"
+		puts "encrypted:       #{@encrypted_message}"
+	end
 end
--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -7,12 +7,12 @@ describe "encryption initialization" do
 	it "sets the user input phrase to input phrase variable" do
 		test_phrase = "I am a word"
 		input_phrase = Encryption.new(test_phrase)
-		expect(input_phrase.phrase).to eq("IAMAWORD")
+		expect(input_phrase.starting_phrase).to eq("IAMAWORD")
 	end
 
 	it "sets phrase to and empty string if no phrase give" do
 		no_phrase = Encryption.new()
-		expect(no_phrase.phrase).to eq("")
+		expect(no_phrase.starting_phrase).to eq("")
 	end
 
 end
@@ -22,23 +22,23 @@ describe "phrase content analysis" do
 	it "tests if phrase is a string" do
 		test_phrase = "I am a word"
 		input_phrase = Encryption.new(test_phrase)
-		expect(input_phrase.phrase).to eq("IAMAWORD")
+		expect(input_phrase.starting_phrase).to eq("IAMAWORD")
 	end
 
 	it "sets phrase to empty string if it is an Array or Number" do
 		test_phrase = ["I am a words", "so am I"]
 		input_phrase = Encryption.new(test_phrase)
-		expect(input_phrase.phrase).to eq("")
+		expect(input_phrase.starting_phrase).to eq("")
 
 		test_phrase = 23423
 		input_phrase = Encryption.new(test_phrase)
-		expect(input_phrase.phrase).to eq("")
+		expect(input_phrase.starting_phrase).to eq("")
 	end
 
 	it "sets phrase to empty string if input contains non-A and Z characters" do
 		test_phrase = "I am words ! am N0t word5"
 		input_phrase = Encryption.new(test_phrase)
-		expect(input_phrase.phrase).to eq("")
+		expect(input_phrase.starting_phrase).to eq("")
 	end
 
 end
@@ -54,7 +54,7 @@ describe "making a random phrase" do
 		#Check that letters do NOT match original input string
 		expect(input_phrase.one_time_phrase).not_to eq("IAMAWORD")
 		#Check that length of original string equals length of one-time-phrase
-		expect(input_phrase.one_time_phrase.length).to eq(input_phrase.phrase.length)
+		expect(input_phrase.one_time_phrase.length).to eq(input_phrase.starting_phrase.length)
 	end
 
 	it "null user input returns empty string" do
@@ -62,6 +62,21 @@ describe "making a random phrase" do
 		input_phrase = Encryption.new()
 		input_phrase.randomize_phrase
 		expect(input_phrase.one_time_phrase).to eq("")
+	end
+
+end
+
+describe "encryption of phrase with one-time-pad" do
+
+	it "encrypted message not empty and different than input and one time pad" do
+		test_phrase = "I am a word"
+		input_phrase = Encryption.new(test_phrase)
+		input_phrase.randomize_phrase
+		input_phrase.encrypt_phrase
+		expect(input_phrase.encrypted_message).not_to eq("")
+		expect(input_phrase.encrypted_message).not_to eq(test_phrase)
+		expect(input_phrase.encrypted_message).not_to eq(input_phrase.one_time_phrase)
+		expect(input_phrase.encrypted_message.length).to eq(test_phrase.length)
 	end
 
 end
