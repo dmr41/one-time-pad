ae9a6efa2b3b7e0aef38bfc998ed23c22cd71f47
David Rivers <dmr41.rivers@gmail.com>
2015-06-01 21:04:59 +0200

    letter randomizer functionality added and working

--- encryption.rb
+++ encryption.rb
@@ -1,23 +1,42 @@
 
 class Encryption
-	attr_accessor :phrase
+	attr_accessor :phrase, :one_time_phrase
 
 ## Encryption class initializes phrase instance variable provided by user
 ## Default state is an empty string
 	def initialize (phrase_to_encode="")
 		@phrase = phrase_content(phrase_to_encode)
+		@one_time_phrase = ""
 	end
 
+## phrase_content takes user input and tests if it is a string and
+## if it only contains a string with A-Z input
 	def phrase_content(phrase_to_encode)
 		if (phrase_to_encode.is_a?(String) && phrase_to_encode != "" )
 			phrase_to_encode.gsub!(/\s+/, "")
 			if(phrase_to_encode[/[a-zA-Z]+/] == phrase_to_encode)
-				@phrase = phrase_to_encode
+				@phrase = phrase_to_encode.upcase
 			end
 		else
 			puts "Your code is not a string between A-Z"
 		end
 			@phrase ||= ""
+	end
+
+## Generate random phrase from user phrase input
+	def randomize_phrase
+
+		if(@phrase.length)
+			for letter in  1..@phrase.length
+				letter_roulet = (65+ rand(25)).chr
+				@one_time_phrase += letter_roulet
+			end
+			puts "Starting phrase: #{@phrase}"
+			puts "One time:        #{@one_time_phrase}"
+		else
+			puts "Phrase is empty. Please try again!"
+		end
+		@one_time_phrase
 	end
 
 end
--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -7,7 +7,7 @@ describe "encryption initialization" do
 	it "sets the user input phrase to input phrase variable" do
 		test_phrase = "I am a word"
 		input_phrase = Encryption.new(test_phrase)
-		expect(input_phrase.phrase).to eq("Iamaword")
+		expect(input_phrase.phrase).to eq("IAMAWORD")
 	end
 
 	it "sets phrase to and empty string if no phrase give" do
@@ -22,7 +22,7 @@ describe "phrase content analysis" do
 	it "tests if phrase is a string" do
 		test_phrase = "I am a word"
 		input_phrase = Encryption.new(test_phrase)
-		expect(input_phrase.phrase).to eq("Iamaword")
+		expect(input_phrase.phrase).to eq("IAMAWORD")
 	end
 
 	it "sets phrase to empty string if it is an Array or Number" do
@@ -35,10 +35,33 @@ describe "phrase content analysis" do
 		expect(input_phrase.phrase).to eq("")
 	end
 
-	it "sets phrase to empty string any letters are not between A and Z" do
+	it "sets phrase to empty string if input contains non-A and Z characters" do
 		test_phrase = "I am words ! am N0t word5"
 		input_phrase = Encryption.new(test_phrase)
 		expect(input_phrase.phrase).to eq("")
+	end
+
+end
+
+describe "making a random phrase" do
+
+	it "randomizes input phrase" do
+		test_phrase = "I am a word"
+		input_phrase = Encryption.new(test_phrase)
+		input_phrase.randomize_phrase
+		#Check that letters have been added to one-time-phrase
+		expect(input_phrase.one_time_phrase).not_to eq(nil)
+		#Check that letters do NOT match original input string
+		expect(input_phrase.one_time_phrase).not_to eq("IAMAWORD")
+		#Check that length of original string equals length of one-time-phrase
+		expect(input_phrase.one_time_phrase.length).to eq(input_phrase.phrase.length)
+	end
+
+	it "null user input returns empty string" do
+		test_phrase = "I am a word"
+		input_phrase = Encryption.new()
+		input_phrase.randomize_phrase
+		expect(input_phrase.one_time_phrase).to eq("")
 	end
 
 end
