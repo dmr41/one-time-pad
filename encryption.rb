738f9e5580500112c344114fd3cc964ea10a901c
David Rivers <dmr41.rivers@gmail.com>
2015-06-01 18:23:55 +0200

    added rspec to project and built phrase validation methods

--- encryption.rb
+++ encryption.rb
@@ -1,16 +1,22 @@
 
 class Encryption
-	attr_accessor :phrase
+	attr_accessor :phrase, :holding
 
 ## Encryption class initializes phrase instance variable provided by user
 ## Default state is an empty string
 	def initialize (phrase_to_encode="")
-		phrase_content(phrase_to_encode)
-		@phrase = phrase_to_encode
+		@phrase = phrase_content(phrase_to_encode)
+		@holding = false
 	end
 
 	def phrase_content(phrase_to_encode)
-
+		@holding = phrase_to_encode.is_a?(String)
+		if (@holding)
+			@phrase = phrase_to_encode
+		else
+			puts "Your code is not a string"
+			@phrase = ""
+		end
 	end
 
 end
--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require './encryption'
 
 
-describe " encryption instance" do
+describe "encryption initialization" do
 
 	it "sets the user input phrase to input phrase variable" do
 		test_phrase = "I am a words"
@@ -13,6 +13,33 @@ describe " encryption instance" do
 	it "sets phrase to and empty string if no phrase give" do
 		no_phrase = Encryption.new()
 		expect(no_phrase.phrase).to eq("")
+	end
+
+end
+
+describe "phrase content analysis" do
+
+	it "tests if phrase is a string" do
+		test_phrase = "I am a words"
+		input_phrase = Encryption.new(test_phrase)
+		expect(input_phrase.phrase).to eq("I am a words")
+	end
+
+	it "sets phrase to empty string if it is an Array or Number" do
+		test_phrase = ["I am a words", "so am I"]
+		input_phrase = Encryption.new(test_phrase)
+		expect(input_phrase.phrase).to eq("")
+
+		test_phrase = 23423
+		input_phrase = Encryption.new(test_phrase)
+		expect(input_phrase.phrase).to eq("")
+	end
+
+	it "sets phrase to empty string any letters are not between A and Z" do
+		test_phrase = "I am words ! am N0t word5"
+		input_phrase = Encryption.new(test_phrase)
+		expect(input_phrase.phrase).to eq("")
+
 	end
 
 end
