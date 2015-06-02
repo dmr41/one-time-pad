9bd5468ec24a0b094a6777c9395c166983663298
David Rivers <dmr41.rivers@gmail.com>
2015-06-02 23:20:15 +0200

    added array spacing functionality to decyrpt and encrypt - needs serious refactor and better tests now.

--- encryption.rb
+++ encryption.rb
@@ -2,7 +2,7 @@
 class Encryption
 	attr_accessor :starting_phrase, :one_time_phrase,
 								:encrypted_message, :starting_array,
-								:one_time_array
+								:one_time_array, :encrypted_array
 
 ## Encryption class initializes phrase instance variable provided by user
 ## Default state is an empty string
@@ -12,6 +12,7 @@ class Encryption
 		@one_time_phrase = ""
 		@one_time_array = []
 		@encrypted_message = ""
+		@encrypted_array = []
 	end
 
 ## phrase_content takes user input and tests if it is a string and
@@ -68,18 +69,24 @@ class Encryption
 		@one_time_phrase = @one_time_array.join(" ")
 	end
 
-	# def encrypt_phrase_array
-	# 	unencoded_array = @starting_phrase.split('').map{|v| v.ord - 65}
-	# 	one_time_array  = @one_time_phrase.split('').map{|v| v.ord - 65}
-	# 	unencoded_array.each_with_index do |starting_value, index|
-	# 		encoding_value = starting_value + one_time_array[index] - 1
-	# 		@encrypted_message += ((encoding_value % 26) + 65).chr
-	# 	end
-	# 	##These output statements will be removed  - For testing only!
-	# 	puts "Starting phrase: #{@starting_phrase}"
-	# 	puts "One time:        #{@one_time_phrase}"
-	# 	puts "encrypted:       #{@encrypted_message}"
-	# end
+
+	def encrypt_phrase_array
+		@starting_array.each_with_index do |word, outer_index|
+			encrypted_word = ""
+			unencoded_array = word.split('').map{|v| v.ord - 65}
+			one_time_array  = @one_time_array[outer_index].split('').map{|v| v.ord - 65}
+			unencoded_array.each_with_index do |starting_value, inner_index|
+				encoding_value = starting_value + one_time_array[inner_index] - 1
+				encrypted_word += ((encoding_value % 26) + 65).chr
+			end
+			@encrypted_array << encrypted_word
+		end
+		@encrypted_message = @encrypted_array.join(" ")
+		##These output statements will be removed  - For testing only!
+		puts "Starting phrase: #{@starting_phrase}"
+		puts "One time:        #{@one_time_phrase}"
+		puts "encrypted:       #{@encrypted_message}"
+	end
 
 ## Encrypts starting message with one time pad value
 	def encrypt_phrase
@@ -117,8 +124,34 @@ class Encryption
 
 	end
 
+	def decrypt_phrase_array(encrypted_message, one_time_pad)
+		@encrypted_message = encrypted_message
+		@encrypted_array = @encrypted_message.split()
+		@one_time_phrase   = one_time_pad
+		@one_time_array = @one_time_phrase.split()
+		@encrypted_array.each_with_index do |word, outer_index|
+			encrypted_array    = word.split('').map{|v| v.ord - 65}
+			one_time_array     = @one_time_array[outer_index].split('').map{|v| v.ord - 65}
+			decoded_word = ""
+			encrypted_array.each_with_index do |encrypted_value, inner_index|
+				decoding_value = encrypted_value - one_time_array[inner_index] +1
+				if(decoding_value >= 0)
+					decoded_word+= (decoding_value + 65 ).chr
+				else
+					decoded_word += ((decoding_value+26) + 65 ).chr
+				end
+			end
+			@starting_array << decoded_word
+		end
+		@starting_phrase = @starting_array.join(" ")
+		##These output statements will be removed  - For testing only!
+		puts "Starting phrase: #{@starting_phrase}"
+		puts "One time:        #{@one_time_phrase}"
+		puts "Encrypted:       #{@encrypted_message}"
+
+	end
+
 end
 
 jimmy = Encryption.new
-jimmy.phrase_content_array("I am me I am")
-jimmy.randomize_phrase_array
+jimmy.decrypt_phrase_array("V WF UB W UH", "O XU JY P VW")
